be62a0efc31496e540ff4d3580310b3e1cf5f787
fabio-bays <92861897+fabio-bays@users.noreply.github.com>
2023-02-09 15:32:35 -0400

    Add files via upload

---
+++ fahsearch.h
@@ -0,0 +1,7 @@
+#ifndef FAHSEARCH_H
+#define FAHSEARCH_H
+
+int linsearch(int *arr, int arr_size, int search_item);
+int binsearch(int *arr, int arr_size, int search_item);
+
+#endif  //  FAHSEARCH_H
